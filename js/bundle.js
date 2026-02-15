07c23dca528b0e0866d3ca3cf8876dd29cfbb997
user-lezi <leleziar@gmail.com>
2026-02-15 22:00:34 +0530

    markdown

--- js/bundle.js
+++ js/bundle.js
@@ -13,7 +13,16 @@
       exports.parseMarkdown = parseMarkdown;
       exports.escapeHtml = escapeHtml;
       function parseMarkdown(input) {
-        return escapeHtml(input).replace(/`([^`]+)`/g, "<code>$1</code>").replace(/\*\*([^*]+)\*\*/g, "<strong>$1</strong>").replace(/\*([^*]+)\*/g, "<em>$1</em>").replace(/\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>').replace(/\n/g, "<br>");
+        const codeBlocks = [];
+        let text = escapeHtml(input).replace(/`([^`]+?)`/g, (_, code) => {
+          const token = `%%CODE_${codeBlocks.length}%%`;
+          codeBlocks.push(`<code>${code}</code>`);
+          return token;
+        }).replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>").replace(/(^|[^*])\*(.+?)\*(?!\*)/g, "$1<em>$2</em>").replace(/~~(.+?)~~/g, "<s>$1</s>").replace(/\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>').replace(/\n/g, "<br>");
+        codeBlocks.forEach((block, i) => {
+          text = text.replace(`%%CODE_${i}%%`, block);
+        });
+        return text;
       }
       function escapeHtml(text) {
         return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
--- js/src/util/markdown.ts
+++ js/src/util/markdown.ts
@@ -1,21 +1,36 @@
 export function parseMarkdown(input: string): string {
-  return (
-    escapeHtml(input)
-      // inline code
-      .replace(/`([^`]+)`/g, "<code>$1</code>")
-      // bold
-      .replace(/\*\*([^*]+)\*\*/g, "<strong>$1</strong>")
-      // italic
-      .replace(/\*([^*]+)\*/g, "<em>$1</em>")
-      // links
-      .replace(
-        /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
-        '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>',
-      )
-      // line breaks
-      .replace(/\n/g, "<br>")
-  );
+  const codeBlocks: string[] = [];
+
+  // Escape HTML first
+  let text = escapeHtml(input)
+    // Extract inline code so we don't parse inside it
+    .replace(/`([^`]+?)`/g, (_, code) => {
+      const token = `%%CODE_${codeBlocks.length}%%`;
+      codeBlocks.push(`<code>${code}</code>`);
+      return token;
+    })
+    // Bold (**text**)
+    .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
+    // Italic (*text*)
+    .replace(/(^|[^*])\*(.+?)\*(?!\*)/g, "$1<em>$2</em>")
+    // Strikethrough (~~text~~)
+    .replace(/~~(.+?)~~/g, "<s>$1</s>")
+    // Links
+    .replace(
+      /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g,
+      '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>',
+    )
+    // Line breaks
+    .replace(/\n/g, "<br>");
+
+  // Restore inline code
+  codeBlocks.forEach((block, i) => {
+    text = text.replace(`%%CODE_${i}%%`, block);
+  });
+
+  return text;
 }
+
 export function escapeHtml(text: string): string {
   return text
     .replace(/&/g, "&amp;")
